895788a77a8e98fb1ba246d7f3dddc2a18cbfb21
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-01-18 10:02:06 -0300

    requisito 4

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const { productsControllers } = require('../controllers');
-// const { validate } = require('../middlewares/validate');
+const validate = require('../middlewares/validateProduct');
 
 const router = express.Router();
 
 router.get('/products', productsControllers.listAllProducts);
 router.get('/products/:id', productsControllers.getByIdProducts);
-router.post('/products', productsControllers.createProducts);
+router.post('/products', validate, productsControllers.createProducts);
 
 module.exports = router;
